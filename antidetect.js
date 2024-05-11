c0b2e6fd367d04ae6a2e5c8f63eb98b01b34e1f2
Blazer <84545189+beforeblazergithub@users.noreply.github.com>
2024-05-11 09:50:14 -0500

    Update and rename code.js to antidetect.js

--- antidetect.js
+++ antidetect.js
@@ -1,16 +1,3 @@
-// ==UserScript==
-// @name         Canvas
-// @namespace    https://github.com/TheLazySquid/canvas-noleave
-// @version      0.5.2
-// @downloadURL  https://github.com/TheLazySquid/canvas-noleave/raw/master/ad.user.js
-// @updateURL    https://github.com/beforeblazergithub/canvas-noleave/raw/master/ad.user.js
-// @description  Prevents canvas from detecing page joins and leaves!
-// @author       blazer
-// @match        https://y115.instructure.com*
-// @grant        none
-// @run-at       document-start
-// ==/UserScript==
-
 Object.defineProperty(document, 'visibilityState', {value: 'visible', writable: true});
 Object.defineProperty(document, 'hidden', {value: false, writable: true});
 Object.defineProperty(document, 'webkitVisibilityState', {value: 'visible', writable: true});
